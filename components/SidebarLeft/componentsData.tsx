8dcaf4091bdbbcc5bdc43d9b60dba95d6877b3a9
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-11-09 22:20:22 +0100

    Sidebar update

---
+++ components/SidebarLeft/componentsData.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+
+const componentsData : any = [
+    {
+        id: 'Typography',
+        
+    },
+    {
+        id: 'Spacing',
+        
+    },
+    {
+        id: 'ColorSystem',
+        
+    },
+    {
+        id: 'Tables',
+        
+    },
+    {
+        id: 'Cards',
+        
+    },
+    {
+        id: 'Dropdown',
+        
+    },
+    {
+        id: 'Tabs',
+        
+    },
+    {
+        id: 'Button',
+        
+    },
+    {
+        id: 'Loading',
+        
+    },
+    {
+        id: 'Toggle',
+        
+    },
+    {
+        id: 'Modal',
+        
+    },
+    {
+        id: 'Input',
+        
+    },
+    {
+        id: 'Text field',
+        
+    },
+    {
+        id: 'Search',
+        
+    },
+    {
+        id: 'Checkboxes',
+        
+    },
+    {
+        id: 'Toaster',
+        
+    },
+    {
+        id: 'Radio Button',
+        
+    },
+    {
+        id: 'Badge',
+        
+    },
+    {
+        id: 'Grid System',
+        
+    },
+   
+
+]
+
+export default componentsData
