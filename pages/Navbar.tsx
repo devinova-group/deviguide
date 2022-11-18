350d29e0de77a28053c8e3cf22ebe594c3f61183
Erik Isaksson <94110856+Erikisak@users.noreply.github.com>
2022-11-18 15:31:58 +0100

    Startpage (#7)

--- pages/Navbar.tsx
+++ pages/Navbar.tsx
@@ -5,20 +5,22 @@ import { Switch, useColorMode } from "theme-ui";
 import img from "./Frame.png";
 import ToggleSwitch from "./Toggle";
 
+
+
 const align = css`
   display: flex;
   align-items: center;
   margin-top: 1rem;
 `;
 const nav = css`
-  background: rgba(0, 0, 0, 0, 2);
-  display: flex;
-  align-items: center;
+    background: rgba(0,0,0,0,2);
+    display: flex;
+    align-items: center;
 `;
 const header = css`
-  width: 100%;
-  display: flex;
-  height: 6rem;
+    width: 100%;
+    display: flex;
+    height: 6rem;   
 `;
 const headerLeft = css`
   display: flex;
@@ -28,51 +30,45 @@ const headerLeft = css`
   cursor: pointer;
 `;
 const link = css`
-  margin-right: 50px;
-  padding-bottom: 10px;
-  border-bottom: 3px solid transparent;
-  font-size: 15px;
-  box-sizing: border-box;
-  cursor: pointer;
-  :hover {
-    border-color: #6044b5;
-  }
+margin-right: 50px;
+padding-bottom: 10px;
+border-bottom: 3px solid transparent;
+font-size: 15px;
+box-sizing: border-box;
+cursor: pointer;
+:hover {
+ border-color: #6044B5;
+ 
+}
 `;
 const toggleAlign = css`
   display: flex;
   align-items: center;
 `;
 
-function NavBar() {
-  return (
-    <header className={header}>
-      <div className={headerLeft}>
-        <Image src={img} alt={""} />
-      </div>
-      <nav className={nav}>
-        <ul className={align}>
-          <Link href={"/components"} className={link}>
-            Components
-          </Link>
-          <Link href={"/coding"} className={link}>
-            Coding
-          </Link>
-          <Link href={"/pages"} className={link}>
-            Pages Design
-          </Link>
-          <Link href={"/design"} className={link}>
-            Design Guide
-          </Link>
-          <Link href={"/about"} className={link}>
-            About Devinova Guides
-          </Link>
-        </ul>
-        <div className={toggleAlign}>
-          <ToggleSwitch></ToggleSwitch>
-        </div>
-      </nav>
-    </header>
-  );
+
+function NavBar () {
+    
+    return (
+        <header className={header}>
+            <div className={headerLeft}>
+            <Image src={img} alt={""}></Image>
+             </div>
+        <nav className={nav}>
+            <ul className={align}>
+                <Link href={""} className={link}>Components</Link>
+                <Link href={""} className={link}>Coding</Link>
+                <Link href={""} className={link}>Pages Design</Link>
+                <Link href={""} className={link}>Design Guide</Link>
+                <Link href={""} className={link}>About Devinova Guides</Link>
+            </ul>
+            <div className={toggleAlign}>
+            <ToggleSwitch ></ToggleSwitch>
+            </div>
+            
+        </nav>
+        </header>
+    )
 }
 
 export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,24 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import { theme } from "../theme/theme";
 import NavBar from "./Navbar";
+import StartPage from "./StartPage";
+import lightmode from "./Background.jpg";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+     <div style={{
+      backgroundImage: `url(${lightmode.src})`,
+      width: '100%',
+      height: '100%',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed'
+    }}
+
+    >
     <ThemeProvider theme={theme}>
       <NavBar />
       <Component {...pageProps} />
     </ThemeProvider>
+    </div>
   );
 }
